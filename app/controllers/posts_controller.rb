0b5614be5bf9314ecb25a0336ac79e72343f25ee
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-12-27 16:30:47 -0300

    CHORE: Add kaminari gem for pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,9 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @posts = Post.all
-  end
-
-  def show
-    @comment = Comment.new
-  end
+def index
+  @posts = Post.page(params[:page]).per(3)
+end
 
   def new
     @post = Post.new
