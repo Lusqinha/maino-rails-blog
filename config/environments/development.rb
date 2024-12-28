75868293dc73cc6155eb02213a7c017b9c8bcd6e
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-12-28 00:34:00 -0300

    FEAT: Configured mailler with SMTP protocol

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,16 @@ Rails.application.configure do
 
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV[ "SMTP_ADDRESS" ],
+    port: ENV[ "SMTP_PORT" ],
+    domain: ENV[ "SMTP_DOMAIN" ],
+    user_name: ENV[ "SMTP_USER_NAME" ],
+    password: ENV[ "SMTP_PASSWORD" ],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
