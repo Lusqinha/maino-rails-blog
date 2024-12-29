0d92ffa5dd905f62f6d5f8431a16c874b0869e77
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-12-29 16:45:40 -0300

    FEAT: adds presence validations for title and body in the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  validates :title, presence: { message: I18n.t("errors.messages.blank") }
+  validates :body, presence: { message: I18n.t("errors.messages.blank") }
 end
