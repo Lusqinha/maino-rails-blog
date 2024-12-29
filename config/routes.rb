eb7d760ced0a28aa672452c63090b22739871165
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-12-29 15:45:38 -0300

    FEAT:  Add support to to en/pt_BR locale on default URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-  sessions: "users/sessions",
-  registrations: "users/registrations",
-  passwords: "users/passwords"
-}
+  scope "(:locale)", locale: /en|pt_BR/ do
+    devise_for :users, controllers: {
+      sessions: "users/sessions",
+      registrations: "users/registrations",
+      passwords: "users/passwords"
+    }
 
-  get "up" => "rails/health#show", as: :rails_health_check
+    get "up" => "rails/health#show", as: :rails_health_check
 
-  root "posts#index"
-  resources :posts do
-    resources :comments, only: [ :create ]
+    root "posts#index"
+    resources :posts do
+      resources :comments, only: [ :create ]
+    end
   end
 end
