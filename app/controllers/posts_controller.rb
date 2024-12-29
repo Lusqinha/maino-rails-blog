a813b7ac5b6ab871dd8fd528b27c91e9614b3446
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-12-29 15:44:14 -0300

    REFACTOR: Formatting adjustments and removal of blank lines

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,6 @@
 class PostsController < ApplicationController
-end
-class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
 def index
   @posts = Post.page(params[:page]).per(3)
