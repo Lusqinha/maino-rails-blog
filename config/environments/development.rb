2cb6429f9d77fd62eded55c217e85848993c805f
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-12-29 17:23:23 -0300

    FEAT: Enables error for missing translations in dev and testing environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
+  config.i18n.raise_on_missing_translations = true
+
   config.i18n.default_locale = :'pt_BR'
   config.i18n.available_locales = [ :'pt_BR', :en ]
 
