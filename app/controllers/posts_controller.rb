555343de831db9a0fbd1eeb99299f8c4dfe89532
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-12-29 23:29:26 -0300

    FEAT: Sort posts by creation date in the list

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    @posts = Post.page(params[:page]).per(3)
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def show
