3bb4f9f2b112e9df856ef3e70725244c941a723f
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-12-31 23:31:34 -0300

    FEATURE: Add Brazilian Portuguese inflections and pluralization rules in Devise

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,42 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym "RESTful"
 # end
+
+ActiveSupport::Inflector.inflections(:'pt_BR') do |inflect|
+  inflect.clear
+
+  inflect.plural(/$/, "s")
+  inflect.plural(/(r|z|n|l)$/i, '\1es')
+  inflect.plural(/al$/i, "ais")
+  inflect.plural(/el$/i, "eis")
+  inflect.plural(/ol$/i, "ois")
+  inflect.plural(/ul$/i, "uis")
+  inflect.plural(/il$/i, "is")
+  inflect.plural(/m$/i, "ns")
+  inflect.plural(/^(japon|escoc|ingl|dinamarqu|fregu|portugu)ês$/i, '\1eses')
+  inflect.plural(/^(|g)ás$/i, '\1ases')
+  inflect.plural(/ão$/i, "ões")
+  inflect.plural(/^(irm|m)ão$/i, '\1ãos')
+  inflect.plural(/^(alem|c|p)ão$/i, '\1ães')
+
+  inflect.plural(/ao$/i, "oes")
+  inflect.plural(/^(irm|m)ao$/i, '\1aos')
+  inflect.plural(/^(alem|c|p)ao$/i, '\1aes')
+
+  inflect.singular(/s$/i, "")
+  inflect.singular(/es$/i, "")
+  inflect.singular(/ais$/i, "al")
+  inflect.singular(/eis$/i, "el")
+  inflect.singular(/ois$/i, "ol")
+  inflect.singular(/uis$/i, "ul")
+  inflect.singular(/is$/i, "il")
+  inflect.singular(/ns$/i, "m")
+  inflect.singular(/sses$/i, "sse")
+  inflect.singular(/^(.*[^s]s)es$/i, '\1')
+  inflect.singular(/ães$/i, "ão")
+  inflect.singular(/ões$/i, "ão")
+  inflect.singular(/ãos$/i, "ão")
+  inflect.singular(/ões$/i, "ão")
+  inflect.singular(/eses$/i, "ês")
+  inflect.singular(/ases$/i, "ás")
+end
