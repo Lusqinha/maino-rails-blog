05c7a860035c230948b8b24adeec5b5e5253c89e
Lucas Borges da Silva <lucas18sborges@gmail.com>
2025-01-02 16:10:40 -0300

    MODEL Add presence and length validation for content in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user, optional: true
+
+  validates :content, presence: { message: I18n.t("errors.messages.blank") }, length: { minimum: 2, maximum: 1000 }
 end
