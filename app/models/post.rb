5e747335f4c2e9a6cbfa4bcf13d6134c0fea3e5e
Lucas Borges da Silva <lucas18sborges@gmail.com>
2025-01-02 00:59:20 -0300

    FIX: Add length validation for title and content in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
-  validates :title, presence: { message: I18n.t("errors.messages.blank") }
-  validates :content, presence: { message: I18n.t("errors.messages.blank") }
+  validates :title, presence: { message: I18n.t("errors.messages.blank") }, length: { minimum: 5 }
+  validates :content, presence: { message: I18n.t("errors.messages.blank") }, length: { minimum: 10 }
 
   def update_tags(tag_names)
     self.tags = tag_names.split(",").map do |name|
