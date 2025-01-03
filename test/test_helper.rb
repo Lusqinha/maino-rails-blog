27645c60ec0b6cb4004e3859ca9dadcdeeb3c321
Lucas Borges da Silva <lucas18sborges@gmail.com>
2025-01-02 22:56:28 -0300

    CHORE: Configure SimpleCov for test coverage reporting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,14 @@
+require "simplecov"
+SimpleCov.start "rails" do
+  add_filter "/bin/"
+  add_filter "/db/"
+  add_filter "/test/"
+  track_files "{app,lib}/**/*.rb"
+end
+
+puts "SimpleCov started, generating coverage reports..."
+
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
