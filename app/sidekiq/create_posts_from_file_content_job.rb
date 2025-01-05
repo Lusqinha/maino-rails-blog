ef4adef4f82ff249a28905bfcbcef835d4a6df3f
Lucas Borges da Silva <lucas18sborges@gmail.com>
2025-01-05 07:28:46 -0300

    FEAT: create a job that create posts from uploaded content

--- app/sidekiq/create_posts_from_file_content_job.rb
+++ app/sidekiq/create_posts_from_file_content_job.rb
@@ -1,7 +1,7 @@
 class CreatePostsFromFileContentJob
   include Sidekiq::Job
 
-  def perform(*args)
-    # Do something
+  def perform(upload_text, user_id)
+    Post.create_from_upload_content(upload_text, user_id)
   end
 end
